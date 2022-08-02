95385439a62f69e90247ca5af536ce79e31ba92a
LinQin <33251559+linqin07@users.noreply.github.com>
2022-08-02 11:54:02 +0800

    Update AkkaQuickstartTest.java
    6

--- src/test/java/com/example/AkkaQuickstartTest.java
+++ src/test/java/com/example/AkkaQuickstartTest.java
@@ -1,25 +1,41 @@
 package com.example;
 
-import akka.actor.testkit.typed.javadsl.TestKitJunitResource;
-import akka.actor.testkit.typed.javadsl.TestProbe;
-import akka.actor.typed.ActorRef;
-import org.junit.ClassRule;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.example.learn.Message;
+import com.example.spring.SpringExtProvider;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//#definition
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AkkaQuickstartTest {
 
-    @ClassRule
-    public static final TestKitJunitResource testKit = new TestKitJunitResource();
-//#definition
+    @Autowired
+    private ActorSystem actorSystem;
 
-    //#test
     @Test
-    public void testGreeterActorSendingOfGreeting() {
-        TestProbe<Greeter.Greeted> testProbe = testKit.createTestProbe();
-        ActorRef<Greeter.Greet> underTest = testKit.spawn(Greeter.create(), "greeter");
-        underTest.tell(new Greeter.Greet("Charles", testProbe.getRef()));
-        testProbe.expectMessage(new Greeter.Greeted("Charles", underTest));
+    public void contextLoads() throws Exception {
+        Props testActor = SpringExtProvider.getInstance().get(actorSystem).create("testActor");
+        akka.actor.ActorRef actorRef = actorSystem.actorOf(testActor);
+        actorRef.tell("hello", akka.actor.ActorRef.noSender());
+        // actorRef.tell("hello1", akka.actor.ActorRef.noSender());
+        // actorRef.tell("hello2", akka.actor.ActorRef.noSender());
+
+        actorRef.tell(new Message("message"), ActorRef.noSender());
+
+        // Props testActor1 = SpringExtProvider.getInstance().get(actorSystem).create("testActor");
+        // akka.actor.ActorRef actorRef1 = actorSystem.actorOf(testActor1);
+        // actorRef1.tell("h2ello", akka.actor.ActorRef.noSender());
+        // actorRef1.tell("h2ello1", akka.actor.ActorRef.noSender());
+        // actorRef1.tell("h2ello2", akka.actor.ActorRef.noSender());
+        System.in.read();
+
     }
-    //#test
 }
